6305191c9bbf6e96b9ef3ce4c2242a408b447e9a
Spookz <andypelico@yahoo.com>
2015-10-10 23:27:05 -0400

    this is the second assignment

--- Assignment2/Assignment2.sh
+++ Assignment2/Assignment2.sh
@@ -3,6 +3,7 @@
 for someimportantforloop in {1..10}
 do
 	cat testing_log.txt > ip_Address_count
+	#the arrow creates a new document.
 done
 #it seems like you can write anything for the name of the for loop as long as it can be reffered to.
 #reminds me of java.............
@@ -14,4 +15,4 @@ sort -f IP_Addresses.txt | uniq -c #as shown in last class, uniq command omits a
 grep -Eo '[0-9|A-F]{1,3}\:[0-9|A-F]{1,3}\:[0-9|A-F]{1,3}\:[0-9|A-F]{1,3}\:[0-9|A-F]{1,3}\:[0-9|A-F]{1,3}' ip_Address_count > MAC_Addresses.txt 
 #This grep command derived from the command before, it is typed up for the the mac addresses to get the letters from the MAC ADDRESSES 
 sort -f MAC_Addresses.txt | uniq -c
-
+#as you can see, the | or piping command makes any 2 commands to run in an order 
